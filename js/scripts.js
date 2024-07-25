f50eda36f5c6e942aabf60aa39bc5792e7617289
Kody Kapetanis <kodykapetanis@yahoo.com>
2024-07-24 21:32:39 -0400

    Create pokedex
    Created a pokemon list and added objects to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-alert('hello world!');
+let pokemonList = [];
 
-let favoriteFood = 'wings';
-document.write(favoriteFood);
+pokemonList[0] = {
+  name: "Bulbasaur",
+  height: 2,
+  weight: 15.2, 
+  types: ["grass", "poison"]
+  
+};
+
+pokemonList[1] = {
+  name: "Charmander",
+  height: 2,
+  weight: 18.7,
+  types: ["fire"]
+};
+
+pokemonList[2] = {
+  name: "Squirtle",
+  height: 1,
+  weight: 19.8,
+  types: ["water"]
+};
+
+console.log(pokemonList);
