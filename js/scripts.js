500f5ee97989eff9a1470d295d90f3a3e00a7c2c
Kody Kapetanis <kodykapetanis@yahoo.com>
2024-08-05 22:13:41 -0400

    PokeList
    wrapped PokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,28 @@
-let pokemonList = [];
+const pokemtonRepository = (function() {
 
-pokemonList[0] = {
-  name: "Bulbasaur",
-  height: 2,
-  weight: 15.2, 
-  types: ["grass", "poison"]
+  const pokemonList = [
+    {name: 'Bulbasur', type: 'grass', height: '2', weight: '15.2'},
+    {name: 'Charmander',type: 'fire', height: '2', weight: '18.7'},
+    {name: 'Squirtle', type: 'water', height: '1', weight: '19.8'}
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(item) {
+    if (typeof item === 'object' && item.name && item.type && item.height && item.weight){
+      pokemonList.push(item);
+    } else {
+      console.log('invalid Pokemon')
+    }
+   
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
   
-};
+})();
 
-pokemonList[1] = {
-  name: "Charmander",
-  height: 2,
-  weight: 18.7,
-  types: ["fire"]
-};
-
-pokemonList[2] = {
-  name: "Squirtle",
-  height: 1,
-  weight: 19.8,
-  types: ["water"]
-};
-
-console.log(pokemonList);
-
-
-pokemonList.forEach(function(pokemon) {
-  document.write(pokemon.name + " (height: " + pokemon.height + ")<br>");
-});
