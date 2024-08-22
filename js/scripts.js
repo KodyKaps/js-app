1acd74dddbccb06d092bff4c0a68d1d52f0714c8
Kody Kapetanis <kodykapetanis@yahoo.com>
2024-08-21 23:01:05 -0400

    API
    Finishing Javascript assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 console.log("JS loaded")
-const pokemonRepository = (function() {
+const pokemonRepository = (function () {
+  //step 2
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-
-
 
   function getAll() {
     return pokemonList;
   }
 
   function add(item) {
-    if (typeof item === 'object' && item.name && item.type && item.height && item.weight) {
+    if (typeof item === 'object' && item.name && item.url) {
       pokemonList.push(item);
     } else {
-      console.log('Invalid Pokémon');
+      console.log('Invalid PokÃ©mon');
     }
   }
 
   function showDetails(pokemon) {
-    console.log(pokemon);
+    
+    loadDetails(pokemon)
   }
 
   function addListItem(pokemon) {
@@ -30,24 +30,56 @@ const pokemonRepository = (function() {
     listItem.appendChild(button);
     pokemonListElement.appendChild(listItem);
 
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
       showDetails(pokemon);
     });
   }
 
   function addAllToList() {
     // debugger
-    getAll().forEach(function(pokemon) {
-      addListItem(pokemon);
-    });
+    loadList().then(response => {
+      //raw response
+      console.log("raw response", response)
+      //parse to json data
+      response.json().then(data =>{
+        console.log("parsed response", data)
+        data.results.forEach(p => addListItem(p))
+      })
+    })
+    .catch(err => {
+      console.error("Api failed")
+    })
+    
+    
+  }
+  //step 3
+  function loadList(){
+    //use fetch to get pokemon
+    //.then waits for resolved
+    return fetch(apiUrl)
+
+  }
+  function loadDetails(pokemon){
+    //use fetch to get pokemon details using the detailsUrl
+    fetch(pokemon.url).then(response => {
+      response.json().then(data =>{
+        console.log("details", data)
+        //put this on a modal and open it
+        return data
+      })
+    })
+
   }
 
   return {
     getAll: getAll,
     add: add,
-    addAllToList: addAllToList
+    addAllToList: addAllToList,
+    loadDetails: loadDetails,
+    loadList: loadList
   };
 
 })();
 
+pokemonRepository.addAllToList();
 pokemonRepository.addAllToList();
